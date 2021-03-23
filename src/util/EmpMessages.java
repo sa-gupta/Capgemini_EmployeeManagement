2b17fb82f606c152c7efced3a1abc635915b135b
Sachin Gupta <sachin070gupta@gmail.com>
2021-03-24 00:14:39 +0530

    Create EmpMessages.java

---
+++ src/util/EmpMessages.java
@@ -0,0 +1,13 @@
+package com.cg.util;
+
+public class EmpMessages {
+
+	public static final String CLASS_NOT_FOUND = "Error in Starting application, try again";
+	public static final String DB_PROP_FILE_NOT_FOUND = "Database property file not found";
+	public static final String DB_ERROR = "Not able to connect to server";
+	public static final String NOT_ABLE_TO_READ_FILE = "Not able to read Database property file";
+	public static final String INSERT_ERROR = "ID already exist, Enter different ID";
+	public static final String UPDATE_ERROR = "ID doesn't exist for updation";
+	public static final String DELETE_ERROR = "ID doesn't exist for deletion";
+
+}
